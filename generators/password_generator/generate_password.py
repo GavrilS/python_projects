06b63537bc89422cf4b7752ec74394d6b245e7f3
GavrilS <gavr1lll@abv.bg>
2026-03-07 08:54:07 +0200

    Finished the password generator script

--- generators/password_generator/generate_password.py
+++ generators/password_generator/generate_password.py
@@ -26,18 +26,42 @@ ALPHABET_CHARACTERS = [
     "t", "u", "v", "w", "x", "y", "z"
 ]
 alphabet_left = 2
-alphabet_length = len(ALPHABET_CHARACTERS)
+ALPHABET_MIN = 2
+ALPHABET_LENGTH = len(ALPHABET_CHARACTERS)
 
 SPECIAL_CHARACTERS = [
     "!", "@", "#", "$", "%", "^", "&", "*", ")", "+", "(", "/", "|"
 ]
 special_characters_left = 2
-special_characters_length = len(SPECIAL_CHARACTERS)
+SPECIAL_CHARACTERS_MIN = 2
+SPECIAL_CHARACTERS_LENGTH = len(SPECIAL_CHARACTERS)
 
-NUMBERS = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+NUMBERS = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"]
 numbers_left = 6
-numbers_length = len(NUMBERS)
+NUMBERS_MIN = 6
+NUMBERS_LENGTH = len(NUMBERS)
 
+CATEGORIES = {
+    0: {
+        "list_item": ALPHABET_CHARACTERS,
+        "char_left": alphabet_left,
+        "char_min": ALPHABET_MIN,
+        "list_len": ALPHABET_LENGTH
+    },
+    1: {
+        "list_item": SPECIAL_CHARACTERS,
+        "char_left": special_characters_left,
+        "char_min": SPECIAL_CHARACTERS_MIN,
+        "list_len": SPECIAL_CHARACTERS_LENGTH
+    },
+    2: {
+        "list_item": NUMBERS,
+        "char_left": numbers_left,
+        "char_min": NUMBERS_MIN,
+        "list_len": NUMBERS_LENGTH
+    }
+}
+TOTAL_MIN_CHARACTERS = 10
 
 
 def main():
@@ -52,10 +76,41 @@ def main():
     except Exception as e:
         print(f"Got an error trying to get the password length set by the user: {e}")
         # print(f"Running with the default password length: {password_length}")
-    print(f"Password length will be {password_length}")
+    # print(f"Password length will be {password_length}")
     flag = True
+    actual_password = ""
+    min_characters_left = TOTAL_MIN_CHARACTERS
     while flag:
-        pass
+        category = CATEGORIES[random.randint(0, 2)]
+        if category["char_left"] <= 0:
+            if (password_length - len(actual_password)) > min_characters_left:
+                # can add password
+                actual_password, min_characters_left = add_character_to_password(actual_password, category, min_characters_left, update_min_characters=False)
+            else:
+                continue
+        else:
+            actual_password, min_characters_left = add_character_to_password(actual_password, category, min_characters_left, update_min_characters=True)
+        
+        if password_length == len(actual_password):
+            flag = False
+    
+    print("Your password is: ", actual_password)
+
+
+def add_character_to_password(password, category, total_min_characters, update_min_characters=True):
+    # print("Category: ", category)
+    character_position = random.randint(0, category["list_len"] - 1)
+    password += category["list_item"][character_position]
+    if update_min_characters:
+        total_min_characters -= 1
+        category["char_left"] -= 1
+    # print("Total min characters left: ", total_min_characters)
+    # print("Password: ", password)
+    # print("Password length: ", len(password))
+    # print("Category: ", category)
+    # print("*" * 100)
+
+    return (password, total_min_characters)
 
 
 if __name__=="__main__":
